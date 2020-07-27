a7be47a8e7500b6f629b00dc30ad09ef2160a557
HyeUngPark <phu4860@gmail.com>
2020-07-27 11:16:56 +0900

    프로젝트 초기화

---
+++ app/src/main/java/com/hyeung/ungstargram/LoginActivity.kt
@@ -0,0 +1,169 @@
+package com.hyeung.ungstargram
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+//import com.facebook.FacebookSdk;
+//import com.facebook.appevents.AppEventsLogger;
+
+class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth ? = null;
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+    var callbackManger : CallbackManager? =null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance();
+        email_login_button.setOnClickListener{
+            signinAndSignup()
+        }
+        google_sigin_button.setOnClickListener(){
+            googleLogin()
+        }
+        facebook_login_button.setOnClickListener(){
+            facebookLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        //        printHashKey()
+        callbackManger = CallbackManager.Factory.create()
+    }
+    fun printHashKey() {
+        try {
+            val info =  packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md =  MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
+    }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManger, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException?) {
+                }
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener{
+                    task->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Login Fail
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManger?.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!=null && result.isSuccess){
+                var account = result?.signInAccount
+                firebaseAuthWithGoofle(account)
+            }
+        }
+    }
+    fun firebaseAuthWithGoofle(account: GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener{
+                    task->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Login Fail
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener{
+            task->
+                if(task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                }else if(task.exception?.message.isNullOrEmpty()){
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }else{
+                    siginEmail()
+                }
+            }
+    }
+    fun siginEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener{
+                task->
+            if(task.isSuccessful){
+                // Login
+                moveMainPage(task.result?.user)
+            }else{
+                // Login Fail
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if(user !=null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/hyeung/ungstargram/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.hyeung.ungstargram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hyeung.ungstargram.navigation.AlarmFragment
+import com.hyeung.ungstargram.navigation.DetailViewFragment
+import com.hyeung.ungstargram.navigation.GridFragment
+import com.hyeung.ungstargram.navigation.UserFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home ->{
+                var detailViewFragment = DetailViewFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                    return true
+            }
+            R.id.action_search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+}
