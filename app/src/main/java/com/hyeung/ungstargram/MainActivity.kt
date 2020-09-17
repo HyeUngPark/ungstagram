f8c3d7515332de4bb92f1ba330c364cc608b74f3
HyeUngPark <phu4860@gmail.com>
2020-09-17 09:18:46 +0900

    상세화면 페이지 추가

--- app/src/main/java/com/hyeung/ungstargram/MainActivity.kt
+++ app/src/main/java/com/hyeung/ungstargram/MainActivity.kt
@@ -49,5 +49,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
         ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+        bottom_navigation.selectedItemId = R.id.action_home
     }
 }
