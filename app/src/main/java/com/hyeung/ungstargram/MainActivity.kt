8d5ad3ab998f340184d199d282dc90949f50255a
HyeUngPark <phu4860@gmail.com>
2020-10-09 10:01:41 +0900

    유저 페이지 작성 및 포스팅 목록 불러오기

--- app/src/main/java/com/hyeung/ungstargram/MainActivity.kt
+++ app/src/main/java/com/hyeung/ungstargram/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.hyeung.ungstargram.navigation.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -39,6 +40,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account ->{
                 var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid",uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
                 return true
             }
